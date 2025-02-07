f403dbb28af5636d2521e99ecf8156f5ac187129
JobertDev27 <jobertdev27@gmail.com>
2025-02-07 19:39:24 +0800

    basic dropdown

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function dropdown () {
-    console.log("yehey i work :>")
+import "./style.css";
+
+function dropdown (main, childArr) {
+    childArr.forEach(elem => main.appendChild(elem));
+    main.classList.add("dropdown-head")
+
+    const toggleDrop = (visible) => {
+        childArr.forEach(elem => elem.classList.toggle("hidden", !visible));
+    }
+
+    main.addEventListener("click", () => toggleDrop(true));
+    main.addEventListener("mouseleave", () => toggleDrop(false));
+
+    toggleDrop(false);
 }
 
 module.exports = dropdown;
